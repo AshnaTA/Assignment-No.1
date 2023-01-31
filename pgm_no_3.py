84aee7e50aac0c2c2499809d7c843849340f3706
AshnaTA <119936723+AshnaTA@users.noreply.github.com>
2023-01-31 20:58:54 +0530

    Create pgm_no_3.py

---
+++ pgm_no_3.py
@@ -0,0 +1,10 @@
+place = input("Enter a place: ")
+
+if place == "beach":
+  print("Relax and swim.")
+elif place == "mountains":
+  print("Go hiking,and rock climbing.")
+elif place == "city":
+  print("Explore museums, and shop.")
+else:
+  print("Enter a valid place.")
